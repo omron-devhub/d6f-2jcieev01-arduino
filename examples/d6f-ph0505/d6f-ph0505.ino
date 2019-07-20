420b1ab236480a1f08afae6a8092830bee788cb8
OMRONEMCSM <github-support-emc-sm-jp-j@omron.com>
2019-07-20 13:59:53 +0900

    format(lint) codes.
    - remove GPIO functions, unrelated with sensors.
    - remove byte/word keywords to be unified with raspberry-pi codes.
    - fix indent, check with cpplint.

--- examples/d6f-ph0505/d6f-ph0505.ino
+++ examples/d6f-ph0505/d6f-ph0505.ino
@@ -21,125 +21,91 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
-
 /* includes */
 #include <Wire.h>
 
-#define GPIO_LED_R_PIN 4
-#define GPIO_LED_G_PIN 5
-#define GPIO_LED_B_PIN 6
-
 /* defines */
-#define  D6F_addr   0x6C // D6F-PH Slave address at 7bit expression
-//#define  RANGE_MODE  100 // +/-50[Pa] range
-#define  RANGE_MODE  250 // 0-250[Pa] range
-//#define  RANGE_MODE 1000 // +/-500[Pa] range
+#define D6F_ADDR 0x6C  // D6F-PH I2C client address at 7bit expression
+// #define RANGE_MODE 100  // +/-50[Pa] range
+#define RANGE_MODE 250      // 0-250[Pa] range
+// #define RANGE_MODE 1000     // +/-500[Pa] range
 
-byte  rbuf[32];
 
-void  I2C_WR(char sadd, byte *dbuf, byte num){
-  int i = 0;
-  
-  Wire.beginTransmission(sadd); // I2C start
-  while (num--){
-    Wire.write(dbuf[i]); // I2C write
-    i++;
-  }
-  Wire.endTransmission(); // I2C stop
+void I2C_WR(char sadd, uint8_t* dbuf, uint8_t num) {
+    int i = 0;
+
+    Wire.beginTransmission(sadd);  // I2C start
+    while (num--) {
+        Wire.write(dbuf[i]);  // I2C write
+        i++;
+    }
+    Wire.endTransmission();  // I2C stop
 }
 
-word I2C_RD_16 (char sadd, byte *dbuf, byte num){
-  int i = 0;
-  word rd_fifo;
-  byte rd_buf[2];
-  
-  Wire.beginTransmission(sadd); // I2C start
-  while (num--){
-    Wire.write(dbuf[i]); // I2C write
-    i++;
-  }
-  Wire.endTransmission(); // I2C stop
+int16_t I2C_RD_16(char sadd, uint8_t* dbuf, uint8_t num) {
+    int i = 0;
+    int16_t rd_fifo;
+    uint8_t rd_buf[2];
 
-  Wire.requestFrom(sadd, 2);
-  i = 0;
-  while(Wire.available()){
-   rd_buf[i++] = Wire.read(); // first received byte stored here
-  }
-  rd_fifo = (word)((rd_buf[0] << 8) | rd_buf[1]);
+    Wire.beginTransmission(sadd);   // I2C start
+    while (num--) {
+        Wire.write(dbuf[i]);        // I2C write
+        i++;
+    }
+    Wire.endTransmission();         // I2C stop
 
-  return rd_fifo;  
+    Wire.requestFrom(sadd, 2);
+    i = 0;
+    while (Wire.available()) {
+        rd_buf[i++] = Wire.read();  // first received byte stored here
+    }
+    rd_fifo = (int16_t)((rd_buf[0] << 8) | rd_buf[1]);
+
+    return rd_fifo;
 }
 
-void setup()
-{
+void setup() {
     Serial.begin(115200);
-    Serial.println("peripherals: GPIO");
-    pinMode(GPIO_LED_R_PIN, OUTPUT);
-    pinMode(GPIO_LED_G_PIN, OUTPUT);
-    pinMode(GPIO_LED_B_PIN, OUTPUT);
-
-    digitalWrite(GPIO_LED_R_PIN, LOW);
-    digitalWrite(GPIO_LED_G_PIN, LOW);
-    digitalWrite(GPIO_LED_B_PIN, LOW);
-
     Serial.println("peripherals: I2C");
-    Wire.begin();  // master
+    Wire.begin();  // i2c master
 
     Serial.println("sensor: Differential pressure Sensor");
     delay(32);
 
-    Wire.begin(); // i2c master
-
-   // EEPROM Control <= 0x00h
-   byte send0[] = {0x0B, 0x00};
-   I2C_WR(D6F_addr, send0, 2);
-
-   // MCU mode <= 0x1F(31) //
- //  byte send1[] = {0x00, 0xD0, 0x47, 0x18, 0x1F}; // <= This line maybe needless for mass production sample.
-  // I2C_WR(D6F_addr, send1, 5);                    // <= This line maybe needless for mass production sample.
-
+    // EEPROM Control <= 0x00h
+    uint8_t send0[] = {0x0B, 0x00};
+    I2C_WR(D6F_ADDR, send0, 2);
 }
-    
+
  /** <!-- loop - Differential pressure sensor {{{1 -->
- * 1. blink LEDs
- * 2. read and convert sensor.
- * 3. output results, format is: [Pa]
+ * 1. read and convert sensor.
+ * 2. output results, format is: [Pa]
  */
-void loop()
-{
-  static bool blink = false;
-  int  i,j;
-  int  itemp;
-  word rd_flow;
-  float Flow_rate;
-  
-  blink = !blink;
-    digitalWrite(GPIO_LED_R_PIN, blink ? HIGH: LOW);
-    digitalWrite(GPIO_LED_G_PIN, blink ? HIGH: LOW);
-    digitalWrite(GPIO_LED_B_PIN, blink ? HIGH: LOW);
+void loop() {
+    int16_t rd_flow;
+    float flow_rate;
+
     delay(900);
 
-  byte send0[] = {0x00, 0xD0, 0x40, 0x18, 0x06};
-  I2C_WR(D6F_addr, send0, 5);
+    uint8_t send0[] = {0x00, 0xD0, 0x40, 0x18, 0x06};
+    I2C_WR(D6F_ADDR, send0, 5);
 
-  delay(50); // wait 50ms
+    delay(50);  // wait 50ms
 
-  byte send1[] = {0x00, 0xD0, 0x51, 0x2C};
-  I2C_WR(D6F_addr, send1, 4);
+    uint8_t send1[] = {0x00, 0xD0, 0x51, 0x2C};
+    I2C_WR(D6F_ADDR, send1, 4);
 
-  byte send2[] = {0x07};
-  rd_flow  = I2C_RD_16(D6F_addr, send2, 1); /* read from [07h] */
+    uint8_t send2[] = {0x07};
+    rd_flow = I2C_RD_16(D6F_ADDR, send2, 1);  // read from [07h]
 
-  
-  if(RANGE_MODE == 250){
-      Flow_rate = ((float)rd_flow - 1024.0) * RANGE_MODE / 60000.0;
-  }
-  else{
-      Flow_rate = ((float)rd_flow - 1024.0) * RANGE_MODE / 60000.0 - RANGE_MODE/2;
-  }
-  Serial.print("sensor output:");
-  Serial.print(Flow_rate, 2); //print converted flow rate
-  Serial.println("[Pa]");
-  ; // post operation.
+
+    if (RANGE_MODE == 250) {
+        flow_rate = ((float)rd_flow - 1024.0) * RANGE_MODE / 60000.0;
+    } else {
+        flow_rate = ((float)rd_flow - 1024.0) * RANGE_MODE / 60000.0 - RANGE_MODE / 2;
+    }
+    Serial.print("sensor output:");
+    Serial.print(flow_rate, 2);  // print converted flow rate
+    Serial.println("[Pa]");
 }
 // vi: ft=arduino:fdm=marker:et:sw=4:tw=80
