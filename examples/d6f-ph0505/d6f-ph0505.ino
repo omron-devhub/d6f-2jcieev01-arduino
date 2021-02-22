40f67f3367e4cfdb86158249ed74d51018ea9763
OMRON Developers Hub <45954736+omron-devhub@users.noreply.github.com>
2021-02-22 19:13:15 +0900

    Update d6f-ph0505.ino

--- examples/d6f-ph0505/d6f-ph0505.ino
+++ examples/d6f-ph0505/d6f-ph0505.ino
@@ -94,10 +94,9 @@ void setup() {
     Serial.println("sensor: Differential pressure Sensor");
     delay(32);
 
-    // D6F setup: EEPROM Control <= 0x00h
+    // 1. Initialize sensor (0Bh, 00h)
     i2c_write_reg16(D6F_ADDR, 0x0B00, NULL, 0);
 }
-
 
 /** <!-- loop - Differential pressure sensor {{{1 -->
  * 1. read and convert sensor.
@@ -105,27 +104,27 @@ void setup() {
  */
 void loop() {
     delay(900);
-
+    
+    // 2. Trigger getting data (00h, D0h, 40h, 18h, 06h)
     uint8_t send0[] = {0x40, 0x18, 0x06};
     i2c_write_reg16(D6F_ADDR, 0x00D0, send0, 3);
 
     delay(50);  // wait 50ms
-
+    
+    // 3. Read data (00h, D0h, 51h, 2Ch) (07h)
     uint8_t send1[] = {0x51, 0x2C};
     i2c_write_reg16(D6F_ADDR, 0x00D0, send1, 2);
-
     uint8_t rbuf[2];
     if (i2c_read_reg8(D6F_ADDR, 0x07, rbuf, 2)) {  // read from [07h]
         return;
     }
     uint16_t rd_flow = conv8us_u16_be(rbuf);
-
     float flow_rate;
+    
     // calculation for +/-50[Pa] range
     flow_rate = ((float)rd_flow - 1024.0) * 100.0 / 60000.0 - 50.0;
 
-    Serial.print("sensor output:");
     Serial.print(flow_rate, 2);  // print converted flow rate
-    Serial.println("[Pa]");
+    Serial.println(" [Pa]");
 }
 // vi: ft=arduino:fdm=marker:et:sw=4:tw=80
